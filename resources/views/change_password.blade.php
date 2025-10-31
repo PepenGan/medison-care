6ba10264c4cdd302d57190d0711167837fd57429
Tepen13 <sm0901068@github.com>
2025-10-31 20:43:59 +0700

    p
    p

---
+++ resources/views/change_password.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body class="antialiased aspect-9-16  font-[inter]"
+  style="--frame: 520px;">
+<p class="">change Password </p>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('views/home', function () {
     return view('home');
 });
 Route::get('views/login', function () {
-    return view('login');
+    return view('Login');
 });
 Route::get('views/register', function () {
     return view('register');
@@ -18,7 +18,7 @@ Route::get('views/register', function () {
 Route::get('views/forgot', function () {
     return view('forgot');
 });
-Route::get('/profile', function () {
+Route::get('views/profile', function () {
     return view('profile');
 });
 Route::get('views/metodepayment', function () {
@@ -26,5 +26,11 @@ Route::get('views/metodepayment', function () {
 });
 Route::get('views/order', function () {
     return view('order');
+});
+Route::get('views/change_password', function () {
+    return view('change_password');
+});
+Route::get('views/change_profile', function () {
+    return view('change_profile');
 });
 
