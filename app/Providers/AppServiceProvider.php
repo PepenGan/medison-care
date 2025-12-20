45f866e1cdb6a881350b4a44975170cc6e263bbc
stephenwijayautama-netizen <stephenwijayautama@gmail.com>
2025-12-20 21:33:03 +0700

    kacau

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,11 +8,12 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        // Tempat bind singleton / interface -> implementation (kalau perlu).
+        //
     }
 
     public function boot(): void
     {
-        // Tempat config global setelah semua provider siap (kalau perlu).
+        //
     }
 }
+
